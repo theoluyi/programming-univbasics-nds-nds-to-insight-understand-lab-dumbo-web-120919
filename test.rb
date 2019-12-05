7fbe090bab14014e4246f90c505078ff191466bc
Theo Carney <theoluyi@gmail.com>
2019-12-05 15:24:35 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -16,6 +16,26 @@
 # the array contains hashes, each of which has 4 keys, one of which is :title
 # we want to collect each one of those titles and print them on separate lines without quotation marks
 # I think that is done by p, not print 
+
+
+outer_results = []
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  inner_results = []
+  while element_index < spice_rack[row_index].count do
+    # How to read the following line of code:
+    #   Array at row_index
+    #   Element of the inner array at element_index
+    #   The first character of that element...
+    if spice_rack[row_index][element_index][0] == "P"
+      inner_results << spice_rack[row_index][element_index]
+    end
+    element_index += 1
+  end
+  outer_results << inner_results
+  row_index += 1
+end       
        
        +{:name=>"Stephen Spielberg",
        + :movies=>
