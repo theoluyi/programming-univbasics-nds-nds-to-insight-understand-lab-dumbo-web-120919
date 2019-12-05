98c56b1bccd8780d1f2bb32c1aff9f447421f5f2
Theo Carney <theoluyi@gmail.com>
2019-12-05 15:49:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,6 +8,10 @@ def pretty_print_nds(nds)
   pp directors_database
 end
 
+
+
+
+
 def print_first_directors_movie_titles
   movie_index = 0 
   all_films = []
@@ -16,12 +20,38 @@ def print_first_directors_movie_titles
     all_films << directors_database[0][:movies][movie_index][:title]
     movie_index += 1
   end
-  p all_films
+  all_films.each do
+    p
+  end 
 end
 
 
 
 
+
+
+
+
+
+
+
+
+# FIRST STAGE, ALMOST SUCCESSFUL, OUTPUT FORMAT NOT CORRECT, MISREMEMBERED WHAT THEY WANTED US TO RETURN, BUT BASICALLY THERE!!!
+# def print_first_directors_movie_titles
+#   movie_index = 0 
+#   all_films = []
+  
+#   while movie_index < directors_database[0][:movies].count do 
+#     all_films << directors_database[0][:movies][movie_index][:title]
+#     movie_index += 1
+#   end
+#   p all_films
+# end
+
+
+
+
+# NOTES
 # pp directors_database[0]
 #   #AoH AoH 
 #   directors_database[0][:movies] # now we're pointing at the array we want to iterate through
