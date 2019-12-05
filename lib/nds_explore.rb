bb25aa9b7da35c48cf3a016904995cd609546d3a
Theo Carney <theoluyi@gmail.com>
2019-12-05 15:42:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,17 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  movie_index = 0 
+  all_films = []
+  
+  while movie_index < directors_database[0][:movies].count do 
+    all_films << directors_database[0][:movies][movie_index][:title]
+    
   pp directors_database[0]
   #AoH AoH 
-  directors_database[0]
+  directors_database[0][:movies] # now we're pointing at the array we want to iterate through
+  directors_database[0][:movies][movie_index][:title] # this is what we add to the collection we return, movie_index is our incrementor. 
+  # while movie_index < directors_database[0][:movies].count do 
   
   
 end
